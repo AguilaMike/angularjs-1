e5b2bf262dd8945278c2afe2641351a45a48dfab
Alberto Basalo <albertobasalo@hotmail.com>
2015-06-26 09:53:55 +0200

    sesion1

---
+++ 04-tres-vistas-y-un-controlador-spa1/server.js
@@ -0,0 +1,67 @@
+"use strict";
+var http = require('http'),
+	url = require('url'),
+	path = require('path'),
+	fs = require('fs');
+
+var mimeTypes = {
+	"html": "text/html",
+	"png": "image/png",
+	"js": "text/javascript",
+	"css": "text/css"
+};
+
+http.createServer(staticServer).listen(3000);
+
+function staticServer(req, res) {
+	// acceder a la url
+	var uri = url.parse(req.url).pathname;
+	console.log('me piden... ' + uri);
+	if (uri === "/") {
+		fileServer(res, "index.html");
+	} else {
+		fileServer(res, uri);
+	}
+
+}
+
+
+function fileServer(res, uri) {
+	// obtener la ruta en disco a aprtir de la ruta web
+	var filename = path.join(process.cwd(), uri);
+	var extension = path.extname(filename).split(".")[1];
+	if (!extension) {
+		extension = "html";
+		filename += "." + extension;
+	}
+	console.log('busco en el disco... ' + filename);
+	fs.exists(filename, function (exists) {
+		if (!exists) {
+			console.log("no encuentro: " + filename);
+			notFound(res);
+		} else {
+			var mimeType = mimeTypes[extension];
+			res.writeHead(200, {
+				'Content-Type': mimeType
+			});
+			var fileStream = fs.createReadStream(filename);
+			fileStream.pipe(res);
+		}
+	});
+}
+
+function notFound(res) {
+	res.writeHead(404, {
+		'Content-Type': 'text/html'
+	});
+	res.write("<html>");
+	res.write("<head>");
+	res.write("<meta charset='utf-8'>");
+	res.write("<title>Hola Mundo</title>");
+	res.write("</head>");
+	res.write("<body>");
+	res.write('<h1>404</h1> Nada por aquí');
+	res.write("</body>");
+	res.write("</html>");
+	res.end();
+}
---
+++ 05-tres-vistas-un-controladory-una-factoria-spa2/cajaCtrl.js
@@ -0,0 +1,29 @@
+(function () {
+    // El controlador ahora tiene una dependencia de la factoría
+    // Las dependencias se buscan en nuestro módulo 
+    // o en cualquiera de sus dependencias
+    var cajaCtrl = function (movimientosFactory,maestrosService) {
+        var vm = this;
+        
+        vm.titulo = "Controla tu Cash Flow";
+        vm.maestros = maestrosService.categarias;
+        vm.nuevoMovimiento = {
+            esIngreso: 1,
+            esGasto: 0,
+            importe: 0,
+            fecha: new Date()
+        };
+        // La parte de datos que debe compartir la delega sobre la factoría
+        vm.movimientos = movimientosFactory.getMovimientos();
+        vm.total = movimientosFactory.getTotal();
+
+        vm.guardarMovimiento = function () {
+            var auxCopyMov = angular.copy(vm.nuevoMovimiento);
+            movimientosFactory.postMovimiento(auxCopyMov);
+            vm.nuevoMovimiento.importe=0;
+        }
+        vm.balance = movimientosFactory.balance();
+        vm.tipo = movimientosFactory.tipo;
+    }
+    angular.module('controlCajaApp').controller('CajaCtrl', cajaCtrl);
+}());
